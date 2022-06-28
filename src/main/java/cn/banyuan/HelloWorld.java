ede52a823c76f1d090c52ca9df028b9e8eadb9cf
xiaolongnv <banyuanxiaolongnv@163.com>
2022-06-28 16:01:17 +0800

    idea  first commit

---
+++ src/main/java/cn/banyuan/HelloWorld.java
@@ -0,0 +1,11 @@
+package cn.banyuan;
+
+/**
+ * @author zhstart
+ * @create 2022-06-28 15:57
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
