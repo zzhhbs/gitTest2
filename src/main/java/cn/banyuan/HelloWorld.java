54a9493da73a7bdd0d0acf5869e6cd08f4d7ce54
xiaolongnv <banyuanxiaolongnv@163.com>
2022-06-28 21:59:28 +0800

    noe more

--- src/main/java/cn/banyuan/HelloWorld.java
+++ src/main/java/cn/banyuan/HelloWorld.java
@@ -6,7 +6,9 @@ package cn.banyuan;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world11111");
         System.out.println("second");
+        System.out.println("banyuan.....");
+        System.out.println("66666");
     }
 }
