bed4dc1d9cc390c0cad60d25976b0ad4daee13e3
xiaolongnv <banyuanxiaolongnv@163.com>
2022-06-28 16:24:02 +0800

    idea  second commit

--- src/main/java/cn/banyuan/HelloWorld.java
+++ src/main/java/cn/banyuan/HelloWorld.java
@@ -7,5 +7,6 @@ package cn.banyuan;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("second");
     }
 }
