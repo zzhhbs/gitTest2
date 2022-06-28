2e5cc5abc03013e7d1d533bfe2472a556043b16b
xiaolongnv <banyuanxiaolongnv@163.com>
2022-06-28 17:12:19 +0800

    word master 第二次提交

--- src/main/java/cn/banyuan/Word.java
+++ src/main/java/cn/banyuan/Word.java
@@ -8,5 +8,6 @@ public class Word {
     public static void main(String[] args) {
         System.out.println("1111111");
         System.out.println("2222222");
+        System.out.println("master1111.....");
     }
 }
