d9f7dbeb8887d861a04c529950ab9045849e34a0
xiaolongnv <banyuanxiaolongnv@163.com>
2022-06-28 17:22:42 +0800

    Hello 第一次提交

---
+++ src/main/java/cn/banyuan/Hello.java
@@ -0,0 +1,11 @@
+package cn.banyuan;
+
+/**
+ * @author zhstart
+ * @create 2022-06-28 17:21
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
