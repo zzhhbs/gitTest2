a8584a278bdf3e36149caa1f2e99ac9b2b176e96
xiaolongnv <banyuanxiaolongnv@163.com>
2022-06-28 21:59:47 +0800

    word 第一次提交  one more

--- src/main/java/cn/banyuan/Word.java
+++ src/main/java/cn/banyuan/Word.java
@@ -8,6 +8,7 @@ public class Word {
     public static void main(String[] args) {
         System.out.println("1111111");
         System.out.println("2222222");
+        System.out.println("one 3333");
         System.out.println("master1111.....");
     }
 }
