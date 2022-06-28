ae8aae0a8a19374a79c8e9f527b27296eb7b61dd
xiaolongnv <banyuanxiaolongnv@163.com>
2022-06-28 17:03:50 +0800

    word 第一次提交

---
+++ src/main/java/cn/banyuan/Word.java
@@ -0,0 +1,12 @@
+package cn.banyuan;
+
+/**
+ * @author zhstart
+ * @create 2022-06-28 17:03
+ */
+public class Word {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+        System.out.println("2222222");
+    }
+}
